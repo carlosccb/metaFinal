7bbf55ea6db9f54e185dc7fff9510f08ac346f6b
Ivan Bonaque Muñoz <i22bomui@uco.es>
2016-05-17 15:29:28 +0200

    Archivo funciones.hpp

---
+++ funciones.hpp
@@ -0,0 +1,17 @@
+
+
+/*------------------------------------------------------------------------------------
+
+
+	Para que no se nos vaya de las manos el tamaño del main, aqui crearemos una
+	funcion por cada uno de los metodos metaheuristicos.
+
+	En cada una de las funciones implementaremos lo que deberia hacer el main
+	para cada metodo, es decir, llevara a cabo el algoritmo con unos parametros
+	y almacenara los datos de la salida en un fichero determinado.
+
+	El main se encargara solo de obtener que tipo de algoritmo queremos ejecutar
+	y con que parametros y llamar a la funcion que deba con un switch.
+
+
+------------------------------------------------------------------------------------*/
