95620ce0516b54c90298a661ab18df2e80302c65
carloscb <carlos.cb.94@gmail.com>
2016-05-18 12:22:15 +0200

    Añadido SolGenerator

---
+++ SolGeneratorMSP.hpp
@@ -0,0 +1,64 @@
+#ifndef __SOL_GENERATOR_MSP_HPP__
+#define __SOL_GENERATOR_MSP_HPP__
+
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+#include "Miscelanea.hpp"
+
+using namespace std;
+
+
+class SolGeneratorMSP{
+
+
+	public:
+
+		/* ----------------------------------------------------
+
+			Funcion que genera una nueva solucion aleatoria 
+		decidiendo el numero de elementos que se incluiran en la
+		mochila, y despues en que posiciones
+
+		----------------------------------------------------- */
+
+		SolutionMSP randomSolutionGenerator(unsigned int objetos) {
+			SolutionMSP solucion(objetos);
+
+/*
+			for(unsigned int i = 0; i < objetos; i++)
+				solucion.setSolution(i, false);
+
+			for(unsigned int i = 0; i < objetos; i++)
+				if (rand()%2 == 1)
+				 solucion.setSolution(i, true);
+*/
+
+
+			int incluidos = rand() % objetos;
+			int pos;
+
+			for (int i = 0; i < incluidos; i++){
+
+				while(true){
+
+					pos = rand() % objetos;
+					if(solucion.getSolution(pos) == false)
+						break;
+
+				}
+
+				solucion.setSolution(pos, true);
+			}
+
+
+			return solucion;
+		}
+		
+
+};
+
+
+#endif
