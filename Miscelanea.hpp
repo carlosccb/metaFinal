1f4cf027e23318727b5c058c60b1a897684cca51
carloscb <carlos.cb.94@gmail.com>
2016-05-18 11:41:30 +0200

    Añadida nueva representacion del problema

---
+++ Miscelanea.hpp
@@ -0,0 +1,27 @@
+#ifndef __MISCELANEA_HPP__
+#define __MISCELANEA_HPP__
+
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+struct problem_element {
+	int clausula1; 
+	int clausula2; 
+	int clausula3;
+};
+
+void printVect(const std::vector<int> &v, std::string separator="", bool endLine=true);
+
+void printList(const std::list<int> &v, std::string separator="", bool endLine=true);
+
+template<class T>
+void printArray(const T &v, std::string separator="", bool endLine=true){
+	for(auto i: v)
+		std::cout << i << separator;
+	if(endLine)
+		std::cout << std::endl;
+}
+
+#endif
