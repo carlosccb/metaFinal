b270688e2594e3b2123e8b6f339231a3e3e7dc8e
carloscb <carlos.cb.94@gmail.com>
2016-05-18 12:31:39 +0200

    Añadidas librerias al main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+#include "GRASPExploratorMSP.hpp"
+#include "InstanceMSP.hpp"
+#include "Miscelanea.hpp"
+#include "SolGeneratorMSP.hpp"
+#include "SolutionMSP.hpp"
+#include "coolingExploratorMSP.hpp"
+#include "funciones.hpp"
+#include "generatorMSP.hpp"
+#include "geneticAlgorithmMSP.hpp"
+#include "localSearchMSP.hpp"
+#include "neighborExploratorMSP.hpp"
+#include "neighborOperatorMSP.hpp"
+
+
 #include <iostream>
 
 using namespace std;
