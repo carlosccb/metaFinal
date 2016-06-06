53a63af3a7d2d1d29cfc514af056e6a8054733c0
carloscb <carlos.cb.94@gmail.com>
2016-06-06 17:14:08 +0200

    Cambiado para nuevo algortimo(s)

--- datosAutomatizado.sh
+++ datosAutomatizado.sh
@@ -25,7 +25,7 @@ do
 	echo " # $num: Usando instancia $inst_name "
 	echo " #" 
 
-	for algo in {1..5};
+	for algo in {1..6};
 	do
 		echo " #  * Algoritmo $algo:"
 		dat_file=$(echo "datFiles/data_${inst_name}_Algoritmo-${algo}_$(date +%s).dat")
