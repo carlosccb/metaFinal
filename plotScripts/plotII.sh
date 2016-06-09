5257518db8577ad40dd66df063373d5a59e64893
carloscb <carlos.cb.94@gmail.com>
2016-06-09 19:34:38 +0200

    Muchas cosas cambiadas

--- plotScripts/plotII.sh
+++ plotScripts/plotII.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-plot() {
-cat << _end_ | gnuplot
-set terminal png giant font arial 12 size 800,600 
-set xlabel "Fitness"
-set ylabel "Weight"
-set key right bottom
-
-set output "../graphics/ComplexAsFuck-CurvesIIV.png"
-plot "../datFiles/ben-weight_Best_EvenLines.txt" using 1:2 title "Best" w p pt 1, \
-"../datFiles/weight.txt" using 1:2 t "Capacity" w l lt rgb "red", \
-"../datFiles/ben-weight_Rand_OddLines.txt" using 1:2 title "Initial" w p pt 1
-_end_
-}
-
 currentBest_vs_bestOverall() {
 #Extract file name and algorithm
 for i in $(ls ../datFiles/*);
@@ -22,7 +8,7 @@ do
 	algorithm=$(echo $i | sed "s/.*data.*\(Algo.*\)_.*/\1/")
 	exec_time=$(echo $i | sed "s/.*\([0-9]\{10,\}\)\.dat/\1/")
 	echo " * Ploting file $file_name with algorithm $algorithm ($exec_time)"
-	graph_name="../graphics/graphic_currentBest_vs_bestOverall-${file_name}-${algorithm}-${exec_time}.png"
+	graph_name="../graphics/graphic_currentBest_vs_bestOverall-${file_name}-${algorithm}-${exec_time}"
 	echo "    -> Plotting to $graph_name"
 	lines=$(wc -l $i | grep -o "^ *[0-9]*" | tr -d "\n| ")
 	echo "    -> Current file has $lines lines. "
@@ -32,7 +18,13 @@ do
 		plot_line="plot for [col=2:3] '$i' using col w p title columnheader"
 	elif [ $lines -gt 100000  ];then
 		#plot_line="plot for [col=2:3] '$i' using col w l title columnheader"
-		plot_line="plot '$i' using 1:2 w l title columnheader, '$i' using 1:3 w l title columnheader"
+		plot_line="plot '$i' using 1:2 w l lc rgb \"forest-green\" title columnheader, '$i' using 1:3 w l lc rgb \"midnight-blue\" title columnheader
+		set samples 10000
+		set output \"${graph_name}_csplines.png\"
+		plot for [col=2:3] '$i' using col smooth csplines title columnheader"
+	elif [ $lines -lt 5000 ];then
+		plot_line="set samples 2000
+		plot for [col=2:3] '$i' using col smooth csplines title columnheader"
 	elif [ $lines -lt 70000 ]; then
 		plot_line="plot for [col=2:3] '$i' using col w l title columnheader"
 	else
@@ -40,15 +32,17 @@ do
 	fi
 
 cat << _end_ | gnuplot
-set terminal png giant font arial 12 size 800,600 
+set lt 1 lc rgb "forest-green" lw 1
+set lt 2 lc rgb "midnight-blue" lw 1
+
+set terminal png giant font arial 12 size 800,600 enhanced background rgb 'beige'
 set xlabel "Iteraciones"
 set ylabel "Fitness"
-set output "$graph_name"
-set termoption dashed
+set key tmargin
+set output "${graph_name}.png"
 $plot_line
 _end_
 done
-
 }
 
 best_for_each_file() {
