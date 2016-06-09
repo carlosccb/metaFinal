a238cd89592e37c7b69205105e653cedfe32a753
carloscb <carlos.cb.94@gmail.com>
2016-06-09 14:00:56 +0200

    Funciona, Añadida funcion mejor para cada instancia

--- plotScripts/plot.sh
+++ plotScripts/plot.sh
@@ -24,18 +24,39 @@ do
 	exec_time=$(echo $i | sed "s/.*\([0-9]\{10,\}\)\.dat/\1/")
 	#algorithm=$(echo $i | sed "s/.*data_.*_\(Algo.*\)_.*/ * algo -> \1/")
 	echo " * Ploting file  $file_name with algorithm $algorithm ($exec_time)"
-	echo
 	graph_name="../graphics/graphic_currentBest_vs_BestOveral_${file_name}_${algorithm}_${exec_time}.png"
 cat << _end_ | gnuplot
 set terminal png giant font arial 12 size 800,600 
 set key outside
 set output "$graph_name"
 
-plot for [col=1:3] '$i' using col smooth csplines title columnheader
+plot for [col=2:3] '$i' using col smooth csplines title columnheader
 
 _end_
 done
 
+}
+
+best_for_each_file() {
+#Extract file name and algorithm
+#Coger fichero de instancias y mirar todos algoritmos para ese fichero
+for i in $(ls ../instances/*);
+do
+	file_name=$(echo $i | sed "s/.*data_\(.*\)_Algo.*/\1/")
+	algorithm=$(echo $i | sed "s/.*data.*\(Algo.*\)_.*/\1/")
+	exec_time=$(echo $i | sed "s/.*\([0-9]\{10,\}\)\.dat/\1/")
+	#algorithm=$(echo $i | sed "s/.*data_.*_\(Algo.*\)_.*/ * algo -> \1/")
+	echo " * Ploting file  $file_name with algorithm $algorithm ($exec_time)"
+	graph_name="../graphics/graphic_currentBest_vs_BestOveral_${file_name}_${algorithm}_${exec_time}.png"
+cat << _end_ | gnuplot
+set terminal png giant font arial 12 size 800,600
+set key outside
+set output "$graph_name"
+
+plot for [col=2:3] '$i' using col smooth csplines title columnheader
+
+_end_
+done
 }
 
 bestOfEach() {
